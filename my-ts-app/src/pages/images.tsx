5f0c12df96b93ccab44ae74e913e4b9ac75c37f5
angelj165 <angelann165@gmail.com>
2025-11-16 01:43:13 -0500

    fix simulation page and added characters and changes css

--- my-ts-app/src/pages/images.tsx
+++ my-ts-app/src/pages/images.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import pumpkinPNG from './images/technica-pumpkin-transparent.png';
-import batPNG from './images/technica-bat-transparent.png';
+import pumpkinPNG from "../images/technica-pumpkin-transparent.png";
+import batPNG from "../images/technica-bat-transparent.png";
+import ghostPNG from "../images/technica-ghost-transparent.png";
 
 interface ImagesProps {
   onClick: () => void;
@@ -9,18 +10,33 @@ interface ImagesProps {
 function Images({ onClick }: ImagesProps) {
   return (
     <div className="images-row">
-      <img
-        src={pumpkinPNG}
-        alt="Pumpkin"
-        className="sim-image"
-        onClick={onClick}
-      />
-      <img
-        src={batPNG}
-        alt="Bat"
-        className="sim-image"
-        onClick={onClick}
-      />
+
+      <div className="image-wrapper" onClick={onClick}>
+        <img src={batPNG} alt="Bat" className="sim-image" />
+        <b><u className="image-caption">High School Student</u></b>
+
+        <p className="image-caption">earning $15/hr</p>
+      </div>
+
+      <div className="image-wrapper" onClick={onClick}>
+        <img src={ghostPNG} alt="Ghost" className="sim-image" />
+        <b><u className="image-caption">College Student</u></b>
+        <p className="image-caption">Earning $15/hr</p>
+        <p className="image-caption">have to pay for tuition</p>
+      </div>
+
+
+      <div className="image-wrapper" onClick={onClick}>
+      <div className="image-container">
+       <img src={pumpkinPNG} alt="Pumpkin" className="sim-image pumpkin" />
+      </div>
+        <div className="caption-container">
+        <p className="image-caption"><b><u>New Grad</u></b></p>
+        <p className="image-caption">Earning $30/hr</p>
+        <p className="image-caption">have to pay for rent and groceries</p>
+      </div>
+    </div>
+
     </div>
   );
 }
