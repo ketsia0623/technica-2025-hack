80ab99b5df886adefc2867b354979209af65b716
Meera <meerasree05@gmail.com>
2025-11-16 10:15:50 -0500

    Fix?

--- my-ts-app/src/pages/HomePage.tsx
+++ my-ts-app/src/pages/HomePage.tsx
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './HomePage.css';
-import Header from '../components/header';
-import Footer from '../components/footer';
-=======
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./HomePage.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
->>>>>>> 90b2b4138fd6a5a00c6c0bd97caaed78c1acd058
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
@@ -18,19 +10,6 @@ const HomePage: React.FC = () => {
   return (
     <div className="home-page">
       <Header />
-<<<<<<< HEAD
-    <div>
-      
-      <h3>
-        Master your finances with our comprehensive dashboard and interactive quiz.
-      </h3>
-      {/* Your box */}
-        <div className="container">
-      <div className="box">
-        <h2>Your financial life, your adventure. 
-          Make choices, see the consequences, 
-          and level up your money skills. </h2>
-=======
 
       <div className="main-content">
         <div className="instructions-box">
@@ -45,7 +24,6 @@ const HomePage: React.FC = () => {
           <button onClick={() => navigate("/dashboard")}>Dashboard</button>
           <button onClick={() => navigate("/quiz")}>Quiz</button>
         </div>
->>>>>>> 90b2b4138fd6a5a00c6c0bd97caaed78c1acd058
       </div>
 
       <Footer />
